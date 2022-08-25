021f5991155b466200735130835999269901fe6d
EchoMan <techechomean93@gmail.com>
2022-08-25 13:59:09 -0700

    update view scope.

--- Reflector.js
+++ Reflector.js
@@ -82,11 +82,6 @@ class Reflector extends THREE.Mesh {
     let renderTarget = null;
 
     this.onBeforeRender = function (renderer, scene, camera) {
-      const gl = renderer.getContext();
-      gl.enable(gl.STENCIL_TEST);
-      gl.stencilFunc(gl.ALWAYS, 0, 0xff);
-      gl.stencilOp(gl.REPLACE, gl.REPLACE, gl.REPLACE);
-
       if ("recursion" in camera.userData) {
         if (camera.userData.recursion === recursion) return;
         camera.userData.recursion++;
@@ -108,136 +103,142 @@ class Reflector extends THREE.Mesh {
       const oldClearAlpha = renderer.getClearAlpha();
 
       // Avoid rendering when reflector is facing away
-      const maxDistance = 20;
-      // if (view.dot(normal) < 0 && view.length() < maxDistance) {
-      view.reflect(normal).negate();
-      view.add(reflectorWorldPosition);
+      const maxDistance = camera.far;
 
-      rotationMatrix.extractRotation(camera.matrixWorld);
+      if (view.dot(normal) < 0 && view.length() < maxDistance) {
+        const gl = renderer.getContext();
+        gl.enable(gl.STENCIL_TEST);
+        gl.stencilFunc(gl.ALWAYS, 0, 0xff);
+        gl.stencilOp(gl.REPLACE, gl.REPLACE, gl.REPLACE);
 
-      lookAtPosition.set(0, 0, -1);
-      lookAtPosition.applyMatrix4(rotationMatrix);
-      lookAtPosition.add(cameraWorldPosition);
+        view.reflect(normal).negate();
+        view.add(reflectorWorldPosition);
 
-      target.subVectors(reflectorWorldPosition, lookAtPosition);
-      target.reflect(normal).negate();
-      target.add(reflectorWorldPosition);
+        rotationMatrix.extractRotation(camera.matrixWorld);
 
-      virtualCamera.position.copy(view);
-      virtualCamera.up.set(0, 1, 0);
-      virtualCamera.up.applyMatrix4(rotationMatrix);
-      virtualCamera.up.reflect(normal);
-      virtualCamera.lookAt(target);
+        lookAtPosition.set(0, 0, -1);
+        lookAtPosition.applyMatrix4(rotationMatrix);
+        lookAtPosition.add(cameraWorldPosition);
 
-      virtualCamera.far = camera.far; // Used in WebGLBackground
+        target.subVectors(reflectorWorldPosition, lookAtPosition);
+        target.reflect(normal).negate();
+        target.add(reflectorWorldPosition);
 
-      virtualCamera.updateMatrixWorld();
-      virtualCamera.projectionMatrix.copy(camera.projectionMatrix);
+        virtualCamera.position.copy(view);
+        virtualCamera.up.set(0, 1, 0);
+        virtualCamera.up.applyMatrix4(rotationMatrix);
+        virtualCamera.up.reflect(normal);
+        virtualCamera.lookAt(target);
 
-      virtualCamera.userData.recursion = 0;
+        virtualCamera.far = camera.far; // Used in WebGLBackground
 
-      // Update the texture matrix
-      textureMatrix.set(
-        0.5,
-        0.0,
-        0.0,
-        0.5,
-        0.0,
-        0.5,
-        0.0,
-        0.5,
-        0.0,
-        0.0,
-        0.5,
-        0.5,
-        0.0,
-        0.0,
-        0.0,
-        1.0
-      );
-      textureMatrix.multiply(virtualCamera.projectionMatrix);
-      textureMatrix.multiply(virtualCamera.matrixWorldInverse);
-      textureMatrix.multiply(this.matrixWorld);
+        virtualCamera.updateMatrixWorld();
+        virtualCamera.projectionMatrix.copy(camera.projectionMatrix);
 
-      // Now update projection matrix with new clip plane, implementing code from: http://www.terathon.com/code/oblique.html
-      // Paper explaining this technique: http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
-      reflectorPlane.setFromNormalAndCoplanarPoint(
-        normal,
-        reflectorWorldPosition
-      );
-      reflectorPlane.applyMatrix4(virtualCamera.matrixWorldInverse);
+        virtualCamera.userData.recursion = 0;
 
-      clipPlane.set(
-        reflectorPlane.normal.x,
-        reflectorPlane.normal.y,
-        reflectorPlane.normal.z,
-        reflectorPlane.constant
-      );
+        // Update the texture matrix
+        textureMatrix.set(
+          0.5,
+          0.0,
+          0.0,
+          0.5,
+          0.0,
+          0.5,
+          0.0,
+          0.5,
+          0.0,
+          0.0,
+          0.5,
+          0.5,
+          0.0,
+          0.0,
+          0.0,
+          1.0
+        );
+        textureMatrix.multiply(virtualCamera.projectionMatrix);
+        textureMatrix.multiply(virtualCamera.matrixWorldInverse);
+        textureMatrix.multiply(this.matrixWorld);
 
-      var projectionMatrix = virtualCamera.projectionMatrix;
+        // Now update projection matrix with new clip plane, implementing code from: http://www.terathon.com/code/oblique.html
+        // Paper explaining this technique: http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
+        reflectorPlane.setFromNormalAndCoplanarPoint(
+          normal,
+          reflectorWorldPosition
+        );
+        reflectorPlane.applyMatrix4(virtualCamera.matrixWorldInverse);
 
-      q.x =
-        (Math.sign(clipPlane.x) + projectionMatrix.elements[8]) /
-        projectionMatrix.elements[0];
-      q.y =
-        (Math.sign(clipPlane.y) + projectionMatrix.elements[9]) /
-        projectionMatrix.elements[5];
-      q.z = -1.0;
-      q.w =
-        (1.0 + projectionMatrix.elements[10]) / projectionMatrix.elements[14];
+        clipPlane.set(
+          reflectorPlane.normal.x,
+          reflectorPlane.normal.y,
+          reflectorPlane.normal.z,
+          reflectorPlane.constant
+        );
 
-      // Calculate the scaled plane vector
-      clipPlane.multiplyScalar(2.0 / clipPlane.dot(q));
+        var projectionMatrix = virtualCamera.projectionMatrix;
 
-      // Replacing the third row of the projection matrix
-      projectionMatrix.elements[2] = clipPlane.x;
-      projectionMatrix.elements[6] = clipPlane.y;
-      projectionMatrix.elements[10] = clipPlane.z + 1.0 - clipBias;
-      projectionMatrix.elements[14] = clipPlane.w;
+        q.x =
+          (Math.sign(clipPlane.x) + projectionMatrix.elements[8]) /
+          projectionMatrix.elements[0];
+        q.y =
+          (Math.sign(clipPlane.y) + projectionMatrix.elements[9]) /
+          projectionMatrix.elements[5];
+        q.z = -1.0;
+        q.w =
+          (1.0 + projectionMatrix.elements[10]) / projectionMatrix.elements[14];
 
-      // Render
-      var currentRenderTarget = renderer.getRenderTarget();
+        // Calculate the scaled plane vector
+        clipPlane.multiplyScalar(2.0 / clipPlane.dot(q));
 
-      if (renderTarget == null) {
-        const outputEncoding =
-          currentRenderTarget === null
-            ? renderer.outputEncoding
-            : currentRenderTarget.texture.encoding;
-        renderTarget = createRenderTarget({
-          encoding: outputEncoding,
-          renderer,
-          scene,
-          camera: virtualCamera,
-        });
+        // Replacing the third row of the projection matrix
+        projectionMatrix.elements[2] = clipPlane.x;
+        projectionMatrix.elements[6] = clipPlane.y;
+        projectionMatrix.elements[10] = clipPlane.z + 1.0 - clipBias;
+        projectionMatrix.elements[14] = clipPlane.w;
+
+        // Render
+        var currentRenderTarget = renderer.getRenderTarget();
+
+        if (renderTarget == null) {
+          const outputEncoding =
+            currentRenderTarget === null
+              ? renderer.outputEncoding
+              : currentRenderTarget.texture.encoding;
+          renderTarget = createRenderTarget({
+            encoding: outputEncoding,
+            renderer,
+            scene,
+            camera: virtualCamera,
+          });
+        }
+
+        this.visible = false;
+        var currentXrEnabled = renderer.xr.enabled;
+        var currentShadowAutoUpdate = renderer.shadowMap.autoUpdate;
+
+        renderer.xr.enabled = false; // Avoid camera modification and recursion
+        renderer.shadowMap.autoUpdate = false; // Avoid re-computing shadows
+        renderer.setRenderTarget(renderTarget);
+        renderer.state.buffers.depth.setMask(true);
+        renderer.setClearColor(whiteColor, 1);
+        if (renderer.autoClear === false) renderer.clear();
+        // // need to update frame to request skeleton update for the renderer skeleton update
+        // // in case of first person view when head gets removed.
+        // renderer.info.render.frame ++;
+        this.composer && this.composer.render();
+
+        renderer.xr.enabled = currentXrEnabled;
+        renderer.shadowMap.autoUpdate = currentShadowAutoUpdate;
+        renderer.setRenderTarget(currentRenderTarget);
+
+        // Restore viewport
+        var viewport = camera.viewport;
+        if (viewport !== undefined) {
+          renderer.state.viewport(viewport);
+        }
+
+        this.visible = true;
       }
-
-      this.visible = false;
-      var currentXrEnabled = renderer.xr.enabled;
-      var currentShadowAutoUpdate = renderer.shadowMap.autoUpdate;
-
-      renderer.xr.enabled = false; // Avoid camera modification and recursion
-      renderer.shadowMap.autoUpdate = false; // Avoid re-computing shadows
-      renderer.setRenderTarget(renderTarget);
-      renderer.state.buffers.depth.setMask(true);
-      renderer.setClearColor(whiteColor, 1);
-      if (renderer.autoClear === false) renderer.clear();
-      // // need to update frame to request skeleton update for the renderer skeleton update
-      // // in case of first person view when head gets removed.
-      // renderer.info.render.frame ++;
-      this.composer && this.composer.render();
-
-      renderer.xr.enabled = currentXrEnabled;
-      renderer.shadowMap.autoUpdate = currentShadowAutoUpdate;
-      renderer.setRenderTarget(currentRenderTarget);
-
-      // Restore viewport
-      var viewport = camera.viewport;
-      if (viewport !== undefined) {
-        renderer.state.viewport(viewport);
-      }
-
-      this.visible = true;
-      // }
 
       renderer.setClearColor(oldClearColor, oldClearAlpha);
     };
